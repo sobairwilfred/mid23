54bd7d4f2e9f29b603eb743aaa242a03b773f8db
Zhenyu <45846924+sobairwilfred@users.noreply.github.com>
2023-11-15 14:20:19 +0100

    Add files via upload

---
+++ Code/Rekurs_basic.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int method(int a){
+    printf("*Method begins with input=%i\n", a);
+    a+=1;
+    if (a<5){
+        method(a);
+    }else {
+        printf("!!! Input >= %i !!!\n", a);
+    }
+    printf("*Method ends with output=%i\n", a);
+    return a;
+}
+
+main(){
+    printf("Main Program Starts!\n");
+    int eingeben=0;
+    method(eingeben);
+    printf("Main Program Ends!\n");
+} 
+
+
+//intuiv zeigt darunter:
+
+/*
+    ******Rekurs1 start
+        ******Rekurs2 start
+            ******Rekurs3 start
+            ******Rekurs3 end
+        ******Rekurs2 end
+    ******Rekurs1 end
+*/
