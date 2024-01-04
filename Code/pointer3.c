1897409b17ed774eb762e8f1f0379b072dc702fa
Zhenyu <45846924+sobairwilfred@users.noreply.github.com>
2024-01-04 19:32:40 +0100

    Beispiel von Pointer: Array

---
+++ Code/pointer3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void ausgabe(int * array, int size) {
+    for(int i = 0; i < size; i++) {
+        printf("Addresse = %p\n", array);
+        printf("Wert = %i\n", *array);
+        *array++;//different to: (*array)++;
+    }
+    printf("\n");
+}
+
+int geteiltDurch(int * array, int size, int divisor) {
+    for (int i=0; i<size; i++){
+        (*array)/=divisor;
+        *array++;
+    }
+}
+
+void main() {
+    int zahlen[] = { 42, 22, 50, 18, 34 };
+    ausgabe(zahlen,5);
+    printf("Geteilt durch %i\n", 2);
+    geteiltDurch(zahlen,5,2);
+    ausgabe(zahlen,5);
+}
