3da6b16c4c3f9c4a36ba2cd1090d15c1aa9587f4
Zhenyu <45846924+sobairwilfred@users.noreply.github.com>
2024-01-04 19:32:04 +0100

    Beispiel von Pointer: dividier

---
+++ Code/pointer2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void dividiere(int a, int b, int * erg, int * rest) {
+    *erg=a/b;
+    *rest=a%b;
+}
+
+void main() {
+    int a = 10;
+    int b = 3;
+    int ergebnis = 0;
+    int rest = 0;
+
+    printf("A = ");
+    scanf("%i", &a);
+    printf("B = ");
+    scanf("%i", &b);
+
+    dividiere(a,b,&ergebnis,&rest);
+    printf("A/B = %i geteilt durch %i ist %i Rest %i\n", a, b, ergebnis, rest);
+
+}
