997e993735268dd1b34bae6bd393f9facfde96c3
Zhenyu <45846924+sobairwilfred@users.noreply.github.com>
2024-02-10 18:48:54 +0100

    Update DivisionDouble_simple.c

--- Code/DivisionDouble_simple.c
+++ Code/DivisionDouble_simple.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*Division mit Fließenkommazahlen*/
-main(){
+int main(){
     int a=5, b=3;
     double c = (double)a / (double)b;
     printf("%i durch %i ist gleich %f", a,b,c);
